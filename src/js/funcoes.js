b74334ba1736da0b67a1317a094a4b9adc85b0cd
gabrielclw <gaburieru_2@hotmail.com>
2016-07-21 04:34:51 -0300

    Validação do botão "Valor" na  página Orçamento

--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -1,12 +1,14 @@
-//Animação SplashScreen
+//Animação SplashScreen + comando mask
 $(document).ready(function(){
 	$(".lista-transition").hide();
     $(".screen-initial").delay(300).fadeOut(1000)
+    $('.input-valor').mask('000.000.000.000.000,00', {reverse: true})
 });
-
+// Interação botão
 $(".buttom-save").click(function() {
+	var value = $(".input-valor").val();
+	if(!value) { $(".input-valor").attr("placeholder", "  Insira o Valor"); } else {
 	$(".screen-transition").delay(0).fadeOut(500)
 	$(".lista-transition").show();
-	var value = $(".input-valor").val(); 
-$('.valor-lista').append(value); })
+$('.valor-lista').append(value); } })
 
