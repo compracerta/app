4035a5dce05d87ca9d604e602e9961f64c477383
gabrielclw <gaburieru_2@hotmail.com>
2016-07-21 17:59:48 -0300

    Morte do modal e Add função ao botão +

--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -4,11 +4,18 @@ $(document).ready(function(){
     $(".screen-initial").delay(300).fadeOut(1000)
     $('.input-valor').mask('000.000.000.000.000,00', {reverse: true})
 });
-// Interação botão
+// Interação botão Orçamento
 $(".buttom-save").click(function() {
 	var value = $(".input-valor").val();
 	if(!value) { $(".input-valor").attr("placeholder", "  Insira o Valor"); } else {
 	$(".screen-transition").delay(0).fadeOut(500)
 	$(".lista-transition").show();
 $('.valor-lista').append(value); } })
+// Função botão + página Lista de Compras
+$(".button-add").click(function(){
+	$( '.body-bottom p' ).remove();
+	$listaEl = $('<li class="lista-compra"><input></input><button class="button-remove glyphicon glyphicon-trash"></button></li>');
+	$listaEl.on("click", ".button-remove" , function(){$(this).parent().remove();});
+    $(".body-bottom ul").append($listaEl);
+})
 
