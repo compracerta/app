1f3ffe57d27d7f0cba0c46470be3d27c015a8b96
gabrielclw <gaburieru_2@hotmail.com>
2016-07-21 21:11:26 -0300

    Add accordion à lista

--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -12,10 +12,20 @@ $(".buttom-save").click(function() {
 	$(".lista-transition").show();
 $('.valor-lista').append(value); } })
 // Função botão + página Lista de Compras
-$(".button-add").click(function(){
+/*$(".button-add").click(function(){
 	$( '.body-bottom p' ).remove();
-	$listaEl = $('<li class="lista-compra"><input></input><button class="button-remove glyphicon glyphicon-trash"></button></li>');
+	$listaEl = $('<li class="lista-compra"><input class="clickable input-lista"></input><button class="button-remove glyphicon glyphicon-trash"></button><div class="wrapper-accordion"><input class="input-qtd"></input><input class="input-qtd"></input></div></li>');
 	$listaEl.on("click", ".button-remove" , function(){$(this).parent().remove();});
     $(".body-bottom ul").append($listaEl);
 })
-
+*/
+var hash = 1;
+$(".button-add").click(function(){
+	$( '.body-bottom p' ).remove();
+	$listaEl = $('<li class="lista-compra"><input class="input-lista"></input><a class="accordion-toggle glyphicon glyphicon-triangle-bottom" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"></a><button class="button-remove glyphicon glyphicon-trash"></button><div id="collapseOne" class="wrapper-accordion collapse in"><input class="input-qtd"></input><input class="input-qtd"></input></div></li>');
+	$listaEl.on("click", ".button-remove" , function(){$(this).parent().remove();});
+    $(".body-bottom ul").append($listaEl);
+    $listaEl.find(".collapse").removeClass("in");
+    $listaEl.find(".accordion-toggle").attr("href",  "#" + (++hash)).text();
+    $listaEl.find(".wrapper-accordion").attr("id", hash).addClass("collapse").removeClass("in");
+})
